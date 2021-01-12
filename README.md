2259cf3bd8c648ac09a410cd2173e222423bfe8c
Abdullah Al-Ashi <>
2021-01-12 20:31:37 +1100

    - added fix to retains cycle and update readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ of iOS Academy reference cycle video tutorial
 found in the link below :
 https://www.youtube.com/watch?v=chI-B8u4MBs
 
-*In this branch the code has a retain cycle and deinit doesn't get called which means the instance is not released  
+*In this branch the code has a retain cycle that has been fixed and deinit is get called which means the instance is released  
--- emptyProject/ViewController.swift
+++ emptyProject/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         let alert = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
         referenceToAlert = alert // this refernce will cause a cycle and will require a weak self in handler
-        alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { _ in
+        alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { [weak self] _ in
+            guard let self = self else { return }
             self.doSomething()
         }))
         present(alert, animated: true, completion: nil)
@@ -23,7 +24,7 @@ class ViewController: UIViewController {
     }
     
     deinit {
-        print("deinit \(self)")
+        print("instance released : \(self)")
     }
 
 }
