b5420554ce5243f89f9c32e9bb9946e3927a6988
Abdullah Al-Ashi <>
2021-01-12 18:00:04 +1100

    - cause a cycle

--- emptyProject/ViewController.swift
+++ emptyProject/ViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var referenceToAlert: UIAlertController!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -9,6 +11,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let alert = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
+        referenceToAlert = alert // this refernce will cause a cycle and will require a weak self in handler
         alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { _ in
             self.doSomething()
         }))
